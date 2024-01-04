030dd0fba91629610fdf1406e130c125065cb281
Daniel Madrid <danimydev@gmail.com>
2024-01-04 17:16:30 -0500

    members route

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import { Hono } from "hono";
 
 import profilesHandler from "./profiles/handler";
 import teamsHandler from "./teams/handler";
+import membersHandler from "./members/handler";
 
 const app = new Hono();
 
 app.route("/profiles", profilesHandler);
 app.route("/teams", teamsHandler);
+app.route("/members", membersHandler);
 
 const port = 3000;
 console.log(`Server is running on port ${port}`);
