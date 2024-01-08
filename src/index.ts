4b7c4c07d0df299f55db1996db51d460517e5757
Daniel Madrid <danimydev@gmail.com>
2024-01-08 17:17:37 -0500

    add routes

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,16 @@ import { Hono } from "hono";
 import profilesHandler from "./profiles/handler";
 import teamsHandler from "./teams/handler";
 import membersHandler from "./members/handler";
+import stadiumsHandler from "./stadiums/handler";
+import matchesHanlder from "./matches/handler";
 
 const app = new Hono();
 
 app.route("/profiles", profilesHandler);
 app.route("/teams", teamsHandler);
 app.route("/members", membersHandler);
+app.route("/stadiums", stadiumsHandler);
+app.route("/matches", matchesHanlder);
 
 const port = 3000;
 console.log(`Server is running on port ${port}`);
