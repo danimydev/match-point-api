7ab3fc3db00035c09b96835a127efd2f8ff2f5ff
Daniel Madrid <danimydev@gmail.com>
2024-01-08 18:46:23 -0500

    add address Stadium prisma schema

--- src/stadiums/schemas.ts
+++ src/stadiums/schemas.ts
@@ -3,9 +3,11 @@ import { object, string } from "valibot";
 export const CreateStadiumSchema = object({
   name: string(),
   picture: string(),
+  address: string(),
 });
 
 export const UpdateStadiumSchema = object({
   name: string(),
   picture: string(),
+  address: string(),
 });
