7d991b3064085d2dfcde0a2789bb87b8e1b7d1cc
Daniel Madrid <danimydev@gmail.com>
2024-01-10 15:11:36 -0500

    prisma singleton

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-export const client = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+}
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const client = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === "production") {
+  globalThis.prisma = client;
+}
